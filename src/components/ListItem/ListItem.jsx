b8e12ce6bf0e51db486ffb24797cd7ce3cd9b3f2
David Kinkaku <davidkinkaku1@gmail.com>
2021-09-09 14:29:19 -0500

    first step done, and adjusted the height in ap.css

---
+++ src/components/ListItem/ListItem.jsx
@@ -0,0 +1,68 @@
+import { useDispatch } from "react-redux";
+import { useState } from "react-redux";
+
+// Displays a single Product with price on the DOM
+function ListItem({ product }) {
+  // Manupilating the toggle
+  const [toggle, setToggle] = useState(true);
+  const dispatch = useDispatch();
+  //   const addProductToCart = () => {
+  //     console.log(product);
+
+  const addItem = () => {
+    dispatch({
+      type: "ADD_TO_CART",
+      payload: product,
+    });
+  };
+
+  const removeItem = () => {
+    dispatch({
+      type: "REMOVE_FROM_CART",
+      payload: product,
+    });
+  };
+
+  return (
+    <>
+      <h2>Step 1: Pick Your Pizza</h2>
+      <div className="pizzaDaddy">
+        <div className="individualMenuItem">
+          {/* picture */}
+          <div>
+            <img src="{product.image_path}"></img>
+          </div>
+
+          {/* menu item title */}
+          <div>
+            <h3>{product.name}</h3>
+          </div>
+
+          {/* description */}
+          <div>
+            <p>{product.description}</p>
+          </div>
+
+          {/* price */}
+          <div>
+            <p>{product.price}</p>
+          </div>
+        </div>
+
+        
+              <div className="btns">
+                    <button onClick={addItem}>Add</button>
+                    <button onClick={removeItem}>Remove</button>
+             </div> 
+
+          {/* toggle for add and remove on the individual item div */}
+          {/* {<div className="menuItem" onClick={ () => setToggle(!toggle) }>
+                {toggle ? <div className="menuDisplay"><p className="singleAddItem" onClick={()=>addItem()}>Add</p></div> :
+                <div><p className="singleRemoveItem" onClick={() =>removeItem()}>Remove</p></div>}</div>} */}
+        
+      </div>
+    </>
+  );
+}
+
+export default ListItem;
--- src/index.js
+++ src/index.js
@@ -3,4 +3,63 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Redux
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { logger } from 'redux-logger';
+
+
+// pizza reducer
+
+const pizzaReducer = (state = [], action) => {
+    if (action.type === 'ADD_NEW_PIZZA') {
+        return [...state, action.payload];
+    }
+    return state;
+};
+
+// customer reducer
+
+let defaultCustomerState = {
+customer_name: "",
+  street_address: "",
+  city: "",
+  zip: "",
+  total: "",
+  type: "",
+}
+
+const customerReducer = (state = defaultCustomerState, action) => {
+    if (action.type === 'ADD_NEW_CUSTOMER') {
+        return [...state, action.payload];
+    }
+    return state;
+};
+
+
+
+// checkout reducer
+const checkoutReducer = (state = [], action) => {
+    if (action.type === 'ADD_TO_CART') {
+        return [...state, action.payload];
+    }
+    else if (action.type === 'CLEAR_CART'){
+        return []
+    }
+    return state;
+};
+
+
+
+// The store is the big JavaScript Object that holds all of the information for our application
+const storeInstance = createStore(
+    combineReducers({
+        pizzaReducer,
+        customerReducer,
+        checkoutReducer
+    }),
+    applyMiddleware(logger)   
+);
+
+// our entire application
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
